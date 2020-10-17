25aa3e50f1e2913485474c096361264f8e8db2eb
Nguyen lam <lamnx1798@gmail.com>
2020-10-17 15:01:00 +0700

    Add Item

--- app/src/main/java/com/example/quizlet/MainActivity.java
+++ app/src/main/java/com/example/quizlet/MainActivity.java
@@ -1,14 +1,52 @@
 package com.example.quizlet;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.example.quizlet.adapter.ItemAdapter;
+import com.example.quizlet.model.Item;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+    private RecyclerView recyclerView;
+    private ArrayList<Item> items;
+    private ItemAdapter adapter;
+    private ImageView checkBtn;
+    private FloatingActionButton addItemBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        items =new ArrayList<>();
+        final ArrayList<String> listDef=new ArrayList();
+        listDef.add("");
+        items.add(new Item("",listDef));
+        recyclerView = findViewById(R.id.rcl_view);
+        adapter  = new ItemAdapter(items, MainActivity.this);
+        recyclerView.setAdapter(adapter);
+        checkBtn=findViewById(R.id.checkBtn);
+        checkBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                items=((ItemAdapter)recyclerView.getAdapter()).getItems();
+            }
+        });
+        addItemBtn=findViewById(R.id.addItemBtn);
+        addItemBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                items=((ItemAdapter)recyclerView.getAdapter()).getItems();
+                ArrayList<String> tempString= new ArrayList<>();
+                tempString.add("");
+                items.add(new Item("",tempString));
+                recyclerView.setAdapter(adapter);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/quizlet/adapter/ItemAdapter.java
+++ app/src/main/java/com/example/quizlet/adapter/ItemAdapter.java
@@ -1,27 +1,34 @@
 package com.example.quizlet.adapter;
 
 import android.content.Context;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.quizlet.MainActivity;
 import com.example.quizlet.R;
 import com.example.quizlet.model.Item;
 
 import java.util.ArrayList;
 
-public class Adapter extends RecyclerView.Adapter<Adapter.Holder>  {
+public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.Holder>  {
+
     ArrayList<Item> items;
     Context context;
 
-    public Adapter(ArrayList<Item> items, Context context) {
+    public ItemAdapter(ArrayList<Item> items, Context context) {
         this.items = items;
         this.context = context;
+    }
+    public ArrayList<Item> getItems() {
+        return items;
     }
 
     @NonNull
@@ -31,8 +38,38 @@ public class Adapter extends RecyclerView.Adapter<Adapter.Holder>  {
     }
 
     @Override
-    public void onBindViewHolder(@NonNull Holder holder, int position) {
-        holder.setContent(items.get(position));
+    public void onBindViewHolder(@NonNull final Holder holder, final int position) {
+        final EditText term=holder.getTermValue();
+        final EditText def=holder.getDefValue();
+        ImageView addBtn=holder.getAddBtn();
+        final RecyclerView defRecycle = holder.getDefRecycle();
+        final ItemDefAdapter adapterr  = new ItemDefAdapter(items.get(position).getDefinition(), context);
+        defRecycle.setAdapter(adapterr);
+        term.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                items.get(position).setTerm(term.getText().toString());
+            }
+        });
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ArrayList<String> listDef=((ItemDefAdapter)defRecycle.getAdapter()).getItems();
+                listDef.add("");
+                items.get(position).setDefinition(listDef);
+                defRecycle.setAdapter(adapterr);
+            }
+        });
     }
 
     @Override
@@ -42,13 +79,27 @@ public class Adapter extends RecyclerView.Adapter<Adapter.Holder>  {
 
     public class Holder extends RecyclerView.ViewHolder {
         EditText tvTerm, tvDefinition;
+        ImageView addBtn;
+        RecyclerView defRecycle;
         public Holder(@NonNull View itemView) {
             super(itemView);
             tvTerm = itemView.findViewById(R.id.tv_term);
             tvDefinition = itemView.findViewById(R.id.tv_definition);
+            defRecycle=itemView.findViewById(R.id.defRecycle);
+            addBtn=itemView.findViewById(R.id.addBtn);
         }
-        public void setContent(Item item){
-
+        public EditText getTermValue(){
+            return tvTerm;
+        }
+        public EditText getDefValue(){
+            return tvDefinition;
+        }
+        public RecyclerView getDefRecycle(){
+            return defRecycle;
+        }
+        public ImageView getAddBtn(){
+            return addBtn;
         }
     }
 }
+
